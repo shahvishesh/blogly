2902a4eeac7418c62a9df675d655c3ef56ec0340
shahvishesh <visheshkshah@gmail.com>
2025-08-03 16:41:21 +0530

    Fix import error in frontend code

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar.jsx'
 import Header from '../components/Header.jsx'
 import BlogList from '../components/BlogList.jsx'
-import Newsletter from '../components/NewsLetter.jsx'
+import Newsletter from '../components/Newsletter.jsx'
 import Footer from '../components/Footer.jsx'
 
 const Home = () => {
